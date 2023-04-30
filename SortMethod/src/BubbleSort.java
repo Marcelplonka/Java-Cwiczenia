ac53a73538c25b625c38e5bb07c1f3c9c33b1945
Marcel Płonka <Marcelp115@onet.pl>
2023-04-30 11:16:51 +0200

    Dodanie metody drukującej i sprawdzającej do klasy Main.

--- SortMethod/src/BubbleSort.java
+++ SortMethod/src/BubbleSort.java
@@ -3,15 +3,22 @@ public class BubbleSort implements SortMethod {
     @Override
     public void sort(int[] arr) {
         int length = arr.length;
+        boolean swapped;
 
         for (int i = 0; i < length - 1; i++) {
+
+            swapped = false;
             for (int j = 0; j < length - 1 - i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j + 1];
                     arr[j + 1] = arr[j];
                     arr[j] = temp;
+                    swapped = true;
                 }
             }
+
+            if (!swapped)
+                break;
         }
     }
 }
--- SortMethod/src/Main.java
+++ SortMethod/src/Main.java
@@ -5,8 +5,37 @@ public class Main {
         SortMethod bubble = new BubbleSort();
 
         int[] arr = {65, 25, 12, 22, 11};
-        bubble.sort(arr);
-        System.out.println(Arrays.toString(arr));
+        boolean sorted = checkIfSorted(arr);
+
+        if (!sorted)
+            bubble.sort(arr);
+
+        printArray(arr);
+    }
+
+    private static void printArray(int[] arr) {
+        System.out.print("[ ");
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1)
+                System.out.print(arr[i] + " ");
+            else
+                System.out.print(arr[i] + ", ");
+        }
+        System.out.print("]");
+    }
+
+    private static boolean checkIfSorted(int[] arr) {
+        int length = arr.length;
+
+        if (length == 1 || length == 0)
+            return true;
+
+        for (int i = 0; i < length - 1; i++) {
+            if (arr[i] > arr[i + 1])
+                return false;
+        }
+
+        return true;
     }
 }
 
